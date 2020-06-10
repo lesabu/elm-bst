8ad87b4cbc80badbe785138b8150b65f90e8d055
Leslie Buettel <lesliebuettel@gmail.com>
2020-06-10 12:34:02 +0200

    refactor

--- src/BST.elm
+++ src/BST.elm
@@ -3,9 +3,6 @@
 
 module BST exposing (BST, create, insert, member)
 
-import Html exposing (a)
-
-
 {-| A binary tree is one of the following
 
   - a leaf
@@ -13,67 +10,85 @@ import Html exposing (a)
 
 -}
 
-type BST a
-    = EmptySlot
-    | Leaf a
-    | Mother a (BST a) (BST a)
+
+type
+    BST comparable
+    -- Nil
+    = Leaf (Maybe comparable)
+    | Mother comparable (BST comparable) (BST comparable)
 
 
-create : a -> BST a
+create : comparable -> BST comparable
 create val =
-    Leaf val
+    Leaf (Just val)
 
 
 myTree : BST Int
 myTree =
     Mother 3
         (Mother 3
-            (Leaf 5)
+            (Leaf (Just 5))
             (Mother 6
-                (Leaf 1)
-                (Leaf 2)
+                (Leaf (Just 1))
+                (Leaf (Just 2))
             )
         )
         (Mother 6
             (Mother 3
                 (Mother 3
-                    (Leaf 5)
+                    (Leaf (Just 5))
                     (Mother 6
-                        (Leaf 1)
-                        (Leaf 2)
+                        (Leaf (Just 1))
+                        (Leaf (Just 2))
                     )
                 )
                 (Mother 6
-                    (Leaf 1)
-                    (Leaf 2)
+                    (Leaf (Just 1))
+                    (Leaf (Just 2))
                 )
             )
-            (Leaf 2)
+            (Leaf (Just 2))
         )
 
+
 tree : BST Int
-tree = Leaf 1
+tree =
+    Leaf (Just 1)
 
 
-insert : a -> BST a -> BST a
+insert : comparable -> BST comparable -> BST comparable
 insert val bst =
     case bst of
-        EmptySlot -> Leaf val
-        Leaf a -> Mother a (create val) EmptySlot
-        -- case matching for Mother to check if it should insert left or right
-        Mother a treeLeft treeRight -> Mother a (insert val treeLeft) (insert val treeRight)
+        Leaf Nothing ->
+            Leaf (Just val)
+
+        Leaf (Just a) ->
+            if val < a then
+                Mother a (Leaf (Just val)) (Leaf Nothing)
+
+            else
+                Mother a (Leaf Nothing) (Leaf (Just val))
+
+        Mother a treeLeft treeRight ->
+            if val < a then
+                Mother a (insert val treeLeft) treeRight
+
+            else
+                Mother a treeLeft (insert val treeRight)
 
 
-member : a -> BST a -> Bool
+member : comparable -> BST comparable -> Bool
 member val bst =
     case bst of
-        EmptySlot -> False
-        Leaf a -> val == a
+        Leaf Nothing ->
+            False
+
+        Leaf (Just a) ->
+            val == a
+
         Mother a treeLeft treeRight ->
-            if a == val then
-                True
-            else if member val treeLeft then
-                True
-            else if  member val treeRight then
-                True
-            else False
+            a == val || member val treeLeft || member val treeRight
+
+
+
+-- create balanced search tree from a given list / hint > recursive function
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,21 @@
 module Main exposing (main)
 
-import Html exposing (div, text)
-import BST exposing (..)
+import BST exposing (BST, create, insert, member)
+import Html exposing (div, p, text)
 
 
 main =
-    div [] [ text "Binary Search Tree" ]
+    div []
+        [ p [] [ text ("is memebr: " ++ Debug.toString (member 78 myBST)) ]
+        , p [] [ text ("my BST: " ++ Debug.toString myBST) ]
+        ]
+
+
+myBST : BST Int
+myBST =
+    create 56
+        |> insert 2
+        |> insert 87
+        |> insert 35
+        |> insert 92
+        |> insert 732
